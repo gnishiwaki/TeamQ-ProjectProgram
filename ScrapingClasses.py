e951f90fcf15c9fa0ad8361c69ba36ed31505d54
gnishiwaki <gen.nishiwaki@gmail.com>
2021-11-05 11:18:04 -0400

    Imported the various libraries necessary to run beautiful soup

--- ScrapingClasses.py
+++ ScrapingClasses.py
@@ -1,1 +1,10 @@
 #This File will be used to scrape files from the classes.uwaterloo page
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+source = requests.get('https://classes.uwaterloo.ca/').text
+
+soup = BeautifulSoup(source, 'lxml')
+
+csv_file = open('cms_scrape.csv', 'w')
